37e3700b79044fe338acb4847b92d192ce5d9ec2
schmidgallm <schmidgallm@outlook.com>
2018-09-29 23:48:04 -0700

    fixed render issue but new issue of texture not loading

--- client/src/components/Globe/Globe.js
+++ client/src/components/Globe/Globe.js
@@ -1,73 +1,99 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component} from 'react';
 import './Globe.css';
+import './earthTexture.jpg';
 import * as THREE from 'three';
 // import OrbitControls from 'three-orbit-controls/index';
-import './earthTexture.jpg';
 
 class Globe extends Component {
-    
-    // will only render after component mounted
-    componentDidMount() {
-        
-    };
-    
-    render() {
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.getElementById('root').appendChild(renderer.domElement)
+   constructor(props) {
+    super(props)
 
-        // event listener for window resize so aspect ratio remains same
-        window.addEventListener('resize', () => {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-            renderer.setSize(width, height);
-            camera.aspect = width / height;
-        });
+    this.start = this.start.bind(this)
+    this.stop = this.stop.bind(this)
+    this.animate = this.animate.bind(this)
+  }
 
-        // init controlls
-        // const controls = OrbitControls(camera, renderer.domElement);
+  componentDidMount() {
+    const width = this.mount.clientWidth
+    const height = this.mount.clientHeight
 
-        // create shape
-        const geometry = new THREE.SphereGeometry(5, 32, 32);
-        const sphereTexture = [
-            new THREE.MeshBasicMaterial({
-                map: new THREE.TextureLoader().load('earthTexture.jpg')
-            })
-        ]
-        // create material, color, or image
-        const material = new THREE.MeshFaceMaterial(sphereTexture);
-        const sphere = new THREE.Mesh(geometry, material);
-        scene.add(sphere);
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      1000
+    )
+    const renderer = new THREE.WebGLRenderer()
+    const geometry = new THREE.SphereGeometry(5, 32, 32);
+    const sphereTexture = [
+        new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load('earthTexture.jpg')
+        })
+    ]
+    const material =  new THREE.MeshFaceMaterial(sphereTexture);
+    const sphere = new THREE.Mesh(geometry, material);
 
-        camera.position.z = 10;
+    camera.position.z = 10;
+    scene.add(sphere);
+    renderer.setClearColor('#2e2e2e');
+    renderer.setSize(width, height);
 
-        // game logic
-        const update = () => {
-            sphere.rotation.x += 0.000;
-            sphere.rotation.y += 0.001;
-        }
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+    this.material = material
+    this.sphere = sphere
 
-        const render = () => {
-            renderer.render(scene, camera);
-        }
+    this.mount.appendChild(this.renderer.domElement)
+    this.start()
 
-        const GameLoop = () => {
-            requestAnimationFrame(GameLoop);
-            update();
-            render();
-        }
-        return (
-            <div>
-            {GameLoop()}
-            </div>
-        )
+    // const controls = new OrbitControls(camera, renderer.domElement);
+
+    window.addEventListener('resize', () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+    });
+  }
+
+  componentWillUnmount() {
+    this.stop()
+    this.mount.removeChild(this.renderer.domElement)
+  }
+
+  start() {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate)
     }
+  }
+
+  stop() {
+    cancelAnimationFrame(this.frameId)
+  }
+
+  animate() {
+    this.sphere.rotation.x += 0.01
+    this.sphere.rotation.y += 0.01
+
+    this.renderScene()
+    this.frameId = window.requestAnimationFrame(this.animate)
+  }
+
+  renderScene() {
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  render() {
+    return (
+      <div
+        style={{ width: '100%', height: '90vh' }}
+        ref={(mount) => { this.mount = mount }}
+      />
+    )
+  }
 }
 
 export default Globe;
