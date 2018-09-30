ecd6d5ba3ef81569d360b34b589982c9638867cf
schmidgallm <schmidgallm@outlook.com>
2018-09-29 22:17:23 -0700

    need a component did mount function to load render. still not working

--- client/src/components/Globe/Globe.js
+++ client/src/components/Globe/Globe.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import './Globe.css';
 import * as THREE from 'three';
+// import OrbitControls from 'three-orbit-controls/index';
 import './earthTexture.jpg';
-// const OrbitControls = require('three-orbitcontrols')(THREE);
-
 
 class Globe extends Component {
-  render() {
     
+    // will only render after component mounted
+    componentDidMount() {
+        
+    };
+    
+    render() {
+
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+        
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        document.getElementById('root').appendChild(renderer.domElement)
 
         // event listener for window resize so aspect ratio remains same
         window.addEventListener('resize', () => {
@@ -22,14 +29,16 @@ class Globe extends Component {
             renderer.setSize(width, height);
             camera.aspect = width / height;
         });
-        
+
         // init controlls
-        // const controls = new OrbitControls(camera, renderer.domElement);
+        // const controls = OrbitControls(camera, renderer.domElement);
 
         // create shape
-        const geometry = new THREE.SphereGeometry(5,32,32);
+        const geometry = new THREE.SphereGeometry(5, 32, 32);
         const sphereTexture = [
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('earthTexture.jpg')})
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load('earthTexture.jpg')
+            })
         ]
         // create material, color, or image
         const material = new THREE.MeshFaceMaterial(sphereTexture);
@@ -40,7 +49,6 @@ class Globe extends Component {
 
         // game logic
         const update = () => {
-            
             sphere.rotation.x += 0.000;
             sphere.rotation.y += 0.001;
         }
@@ -56,11 +64,10 @@ class Globe extends Component {
         }
         return (
             <div>
-                {GameLoop()}
+            {GameLoop()}
             </div>
-        );
-        
-  }
+        )
+    }
 }
 
 export default Globe;
