a853e1c1fdb05a5e47e676264b0a15cd89aa5264
stevenpassey <steven.passey.live@gmail.com>
2018-10-13 13:58:09 -0700

    Added globe zoom animation, and user login session detection on page load

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { Component} from 'react';
+import API from '../../utils/API';
 import './LoginButton.css';
 
-const LoginButton = (props) => (
-  <button className={props.panelShown ? "login-button login-button__panelShown" : "login-button"} onClick={props.cbProp}>Login / Register</button>
-);
+
+class LoginButton extends Component {
+
+	componentDidMount()
+	{
+		API.session().then(response => {
+			if(response.data.username)
+			{
+				this.value = response.data.username;
+			}
+			else
+			{
+				this.value = "Login / Register";
+			}
+				
+              
+        })
+        .catch(err => {
+          console.log(err);
+        });
+	}
+	
+	value = "";
+	
+	render(){
+		return (
+			<button className={this.props.panelShown ? "login-button login-button__panelShown" : "login-button"} onClick={this.props.cbProp}>{this.value}</button>
+		);
+	}
+}
+
 
 export default LoginButton;
